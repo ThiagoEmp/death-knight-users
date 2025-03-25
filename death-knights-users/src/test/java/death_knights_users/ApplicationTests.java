b7768d348390a78ba4cfa7acd9c69e557cf0ac6d
Thiago Barcellos <111370569+ThiagoEmp@users.noreply.github.com>
2025-03-25 19:44:31 -0300

    Add files via upload

---
+++ death-knights-users/src/test/java/death_knights_users/ApplicationTests.java
@@ -0,0 +1,13 @@
+package death_knights_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
